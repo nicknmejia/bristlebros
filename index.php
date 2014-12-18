56ac7dd1815205052ca95155a0a791af9f26dd0f
Nicholas Mejia <nicknmejia@gmail.com>
2014-12-18 14:53:23 -0800

    First build of bristle brothers simple landing

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Bristle Brothers</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" type="text/css" href="css/user_styles.css">
+    <link href='http://fonts.googleapis.com/css?family=Great+Vibes|IM+Fell+English|Bowlby+One+SC|Ewert' rel='stylesheet' type='text/css'>
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+    
+    <form>
+      <div class="form row">
+        <div class="large-12 large-centered columns line">
+          <h1 class="header-text">BRISTLE BROTHERS</h1>
+        </div>
+        <div class="large-12 large-centered columns">
+          <h1 class="header-text2">- Fine Dental Care Producte -</h1>
+        </div>
+        <div class="large-12 large-centered columns">
+          <div class="centered">
+            <h4 class="button alert round">Sign up below for more info:
+          </div>
+        </div>
+        <div class="large-6 large-centered columns">
+          <div class="centered">Please provide the following info:</div>
+          <div class="large-6 columns">
+            <input type="text" placeholder="First Name">
+            <input type="text" placeholder="Last Name">
+          </div>
+          <div class="large-6 columns">
+            <input type="text" placeholder="Email">
+            <input type="text" placeholder="Zip">
+          </div>
+          <div class="centered">
+            <h4 class="button success radius">Submit</h4>
+          </div>
+        </div>
+      </div>  
+    </form>
+    
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
