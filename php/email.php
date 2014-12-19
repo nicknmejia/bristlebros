b10b586c0bbb945c914cf06843a87537e68e5c42
Nicholas Mejia <nicknmejia@gmail.com>
2014-12-19 12:27:39 -0800

    Ignoring email script.  Contains sensetive info.  Adding dummy file

--- php/email.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db = new PDO('mysql:host=localhost;dbname=testdb;charset=utf8', 
-			  'username',
-			  'password', 
-			  array(PDO::ATTR_EMULATE_PREPARES => false, 
-			  	    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
-?>
